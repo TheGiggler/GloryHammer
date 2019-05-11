943a1c1b506d664610bc33ae5625f8b8fa19b09a
Mike Tocci <maddog19rw@gmail.com>
2019-05-10 22:04:31 -0400

    geo service is fetching info from an api (ipstack)

---
+++ ConcreteGeolocationService/Network/Service.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ConcreteGeolocationService.Network
+{
+    internal class NetworkService
+    {
+        internal struct FetchResult//should be in a models namespace
+        {
+            public bool Success {get;set;}
+            public IPData Data { get; set; }
+        }
+        internal static async Task<FetchResult> FetchEndpointData(string geoApiUrl,string apiKey, string endpoint)
+        {
+            string endpointToTest;
+            var result = new FetchResult();
+
+            //determine if this is ip address or domain
+            //if domain, need to get ip first
+            IPAddress output;
+
+            if (!IPAddress.TryParse(endpoint, out output))
+            {
+                endpointToTest = GetIpAddressFromDomain(endpoint);
+            }
+            else
+            {
+                endpointToTest = endpoint;
+            }
+
+            var target = string.Format(geoApiUrl, endpointToTest, apiKey);
+            HttpClient client = new HttpClient();
+
+            //add Polly retries with back-off here
+            var response = await client.GetAsync(target);
+
+            if (response.IsSuccessStatusCode)
+            {
+                result.Success = true;
+                //deserialize content into IPData
+                JsonSerializerSettings jSettings = new JsonSerializerSettings();
+                jSettings.NullValueHandling = NullValueHandling.Ignore;
+                result.Data= JsonConvert.DeserializeObject<IPData>(response.Content.ReadAsStringAsync().Result,jSettings);
+            }
+            else
+            {
+                result.Success = false;
+            }
+
+            return result;
+        }
+
+        private static string GetIpAddressFromDomain(string endpoint)
+        {
+            IPAddress[] ips =  Dns.GetHostAddresses(endpoint);
+            //this can return multiples... just take the first for our purposes
+            if (ips.Length > 0)
+            {
+                return ips[0].ToString();
+            }
+            else
+            {
+                throw new ArgumentException("Valid domain or IP address was not submitted.");
+            }
+        }
+    }
+}
--- ConcreteGeolocationService/Program.cs
+++ ConcreteGeolocationService/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using ConcreteGeolocationService.Network;
+using Nito.AsyncEx.Synchronous;
 
+/// <summary>
+/// Service to look up
+/// </summary>
 namespace ConcreteGeolocationService
 {
+    internal class ServiceSettings
+    {
+
+
+        public ServiceSettings(NameValueCollection settings)
+        {
+            this.GeoServiceApiKey = settings["GeoServiceApiKey"];
+            this.GeoServiceName = settings["GeoServiceName"];
+            this.GeoServiceUrl = settings["GeoServiceUrl"];
+        }
+
+        public string GeoServiceUrl { get; set; }
+        public string GeoServiceName { get; set; }
+        public string GeoServiceApiKey { get; set; }
+    }
+
     class Program
     {
+        static ServiceSettings settings;
+        static string endpointToTest;
+
         static void Main(string[] args)
         {
+            endpointToTest = args[0];
+            //TODO test for missing
+            settings = LoadSettingsFromConfig();
             Console.WriteLine("Hello World!");
+
+            //here we'd set up grpc server to listen
+            //for now, just this
+            var task = NetworkService.FetchEndpointData(settings.GeoServiceUrl, settings.GeoServiceApiKey, endpointToTest);
+
+            var result = task.WaitAndUnwrapException();
+         
+        }
+
+        private static ServiceSettings LoadSettingsFromConfig()
+        {
+            //Would prefer "Fourth approach" here: https://www.c-sharpcorner.com/article/four-ways-to-read-configuration-setting-in-c-sharp/
+            //throw new NotImplementedException();
+            var settings = ConfigurationManager.GetSection("ServiceSettings") as NameValueCollection;
+
+            return new ServiceSettings(settings);
+
+
         }
     }
 }
